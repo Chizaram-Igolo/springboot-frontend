18e8e648ce28b56f198726f6bbca72708be1f8c9
Chizaram-Igolo <chizaram.igolo@yahoo.com>
2021-05-27 23:31:42 +0100

    add static.json

--- src/App.js
+++ src/App.js
@@ -1,55 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useEffect, useState } from 'react'
-import SongCard from './SongCard';
-import Container from 'react-bootstrap/Container'
-import Row  from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import SongCard from "./SongCard";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const initialState = {
   isLoading: true,
-  songs: []
-}
+  songs: [],
+};
 const App = () => {
-
-
-  const [data, setData] = useState(initialState)
+  const [data, setData] = useState(initialState);
 
   const getData = async () => {
-    const response = await fetch('/api/songs');
-    const body = await response.json()
-    setData({ songs: body, isLoading: false })
-  }
+    const response = await fetch("/api/songs");
+    const body = await response.json();
+    setData({ songs: body, isLoading: false });
+  };
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
-
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Container>
-        
           <div className="App-intro">
-          <h2 style={{color: 'white'}}>Song List</h2>
-          <hr></hr>
-          <Row>
-          {data.songs.map(song =>
-              <Col>
-                 <SongCard key={song.id} song={song} />
-              </Col>
-           
-          )}
-          </Row>
-        </div>
-          
-      </Container>
-        
+            <h2 style={{ color: "white" }}>Song List</h2>
+            <hr></hr>
+            <Row>
+              {data.songs.map((song) => (
+                <Col>
+                  <SongCard key={song.id} song={song} />
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Container>
       </header>
     </div>
-  )
-}
+  );
+};
 export default App;
